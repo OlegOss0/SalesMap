d6251aaa551fe73ac95933368c80b67abc71070e
OlegOss0 <oleg-asb@bk.ru>
2018-05-23 04:57:18 +0300

    create_ui_login_activity_add_files

---
+++ app/src/main/java/e/sergeev/oleg/salesmap/LoginAcrivity1.kt
@@ -0,0 +1,14 @@
+package e.sergeev.oleg.salesmap
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by o.sergeev on 23.05.2018.
+ */
+class LoginAcrivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login1)
+    }
+}
